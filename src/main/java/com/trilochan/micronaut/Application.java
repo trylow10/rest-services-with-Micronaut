f1685b99a5bdf4806d96c5a8906f99fcb203a484
Trilochan Aryal <trylow10>
2022-09-08 01:43:26 +0000

    Initial commit

---
+++ src/main/java/com/trilochan/micronaut/Application.java
@@ -0,0 +1,9 @@
+package com.trilochan.micronaut;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
